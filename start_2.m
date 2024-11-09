b5fae585c038e54968ba68d9ac38aa2bb43da9b7
ShadmanAkhter123 <shadman.akhter123@gmail.com>
2024-11-09 22:53:19 +0600

    2nd day

---
+++ start_2.m
@@ -0,0 +1,13 @@
+a= [2 1 3 2 1 0 5 8];
+b= [1:8];
+element_mult= a.*b;
+norm_mult= a * b';
+final_value = element_mult.*norm_mult;
+root_fv= sqrt(final_value)
+sz= size(element_mult);
+po=a(end);
+p = root_fv.^po
+sum= p+sz(end)
+M= mean(sum) 
+
+%%doc mean
