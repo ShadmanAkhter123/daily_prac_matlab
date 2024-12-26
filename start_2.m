0798adfa51c4a8364ce84bce85082149dfc19050
ShadmanAkhter123 <shadman.akhter123@gmail.com>
2024-12-26 23:04:27 +0600

    back on it babe

--- start_2.m
+++ start_2.m
@@ -1,13 +1,15 @@
+clc;
 a= [2 1 3 2 1 0 5 8];
 b= [1:8];
-element_mult= a.*b;
-norm_mult= a * b';
+element_mult= a.*b
+norm_mult= a * b'   %b' is transpose here a*transpose(b)
 final_value = element_mult.*norm_mult;
-root_fv= sqrt(final_value)
+root_fv= sqrt(final_value);
 sz= size(element_mult);
 po=a(end);
 p = root_fv.^po
 sum= p+sz(end)
 M= mean(sum) 
 
-%%doc mean
+%doc mean
+
